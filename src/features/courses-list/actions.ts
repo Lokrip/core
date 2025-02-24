79f85e25750f941495704ee3a69001288278a6e9
Lokrip <165576212+Lokrip@users.noreply.github.com>
2025-02-24 17:08:34 +0600

    feat: add course form

---
+++ src/features/courses-list/actions.ts
@@ -0,0 +1,18 @@
+"use server"
+
+import { revalidatePath } from "next/cache"
+import { coursesRepository } from "./courses.repository"
+
+export const createCourseAction = async (
+    command: CreateCourseListElementCommand,
+    revalidatePagePath: string,
+) => {
+    await coursesRepository.createCourseElement(command)
+    // Когда вызывается revalidatePath('/some-page'), 
+    // Next.js очищает серверный кэш для указанного пути и 
+    // заставляет сервер повторно сгенерировать контент при 
+    // следующем запросе. Это полезно, если данные на странице изменились 
+    // (например, добавлен новый курс) и нужно отобразить свежие 
+    // данные без перезагрузки всего приложения.
+    revalidatePath(revalidatePagePath)
+}
--- src/features/courses-list/pub/create-course-form.tsx
+++ src/features/courses-list/pub/create-course-form.tsx
@@ -4,15 +4,18 @@ import { Button, FormControl, FormHelperText, Input, InputLabel, TextareaAutosiz
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { FormEvent, FormEventHandler } from "react";
+import { FormEvent, FormEventHandler, useTransition } from "react";
+import { createCourseAction } from "../actions";
 
 const createCourseFormSchema = z.object({
     name: z.string().min(2).max(45),
     description: z.string()
 })
 
-export function CreateCourseForm() {
+export function CreateCourseForm({revalidatePagePath}: {revalidatePagePath: string}) {
     //zodResolver — это функция из @hookform/resolver/zod, которая позволяет использовать валидацию схем Zod вместе с react-hook-form.
+    const [isCreateTransition, startCreateTransition] = useTransition()
+    
     const {handleSubmit,} = useForm({
         resolver: zodResolver(createCourseFormSchema),
         defaultValues: {
@@ -20,15 +23,14 @@ export function CreateCourseForm() {
             description: ""
         }
     })
-
-    const onSubmit: SubmitHandler<FieldValues> = (data) => {
-        console.log(data);
-    }
-    
     
     
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit((data) => {
+            startCreateTransition(async () => {
+                createCourseAction(data, revalidatePagePath);
+            })
+        })}>
             <div className="mt-5">
                 <FormControl>
                     <InputLabel htmlFor="my-input">Название</InputLabel>
@@ -43,7 +45,7 @@ export function CreateCourseForm() {
                     <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
                 </FormControl>
             </div>
-            <Button variant="contained" style={{width: "100%"}}>Submit</Button>
+            <Button type="submit" disabled={isCreateTransition} variant="contained" style={{width: "100%"}}>Добавить</Button>
         </form>
     )
 }
